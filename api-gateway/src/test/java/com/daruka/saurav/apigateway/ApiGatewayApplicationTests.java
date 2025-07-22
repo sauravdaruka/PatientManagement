43830908237456df8132a1a1555ed119c218b00b
sauravdaruka <sauravdaruka2010@gmail.com>
2025-07-22 20:29:55 +0530

    Added Api Gateway Module

---
+++ api-gateway/src/test/java/com/daruka/saurav/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daruka.saurav.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
